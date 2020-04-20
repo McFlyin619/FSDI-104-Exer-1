6f9b6ddbee72be6c9e8e270163c6d60fd6b7942a
OBrien McQuade <62272369+McFlyin619@users.noreply.github.com>
2020-04-19 18:41:38 -0700

    Created Script AVG
    Created an AVG script for Exercise 1

---
+++ script.js
@@ -0,0 +1,9 @@
+var grades = [87, 92, 84, 79, 91, 77]
+
+alert("Grades: " + grades);
+
+let sum = grades[0]+grades[1]+grades[2]+grades[3]+grades[4]+grades[5];
+
+let count = 6;
+
+alert("Avg: " + sum/count);
